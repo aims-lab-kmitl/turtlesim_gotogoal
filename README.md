8ffe3ef1233f9a7c43de84977ea8648f040f60d7
ThitipongTh <james.ttpts@gmail.com>
2022-09-16 14:23:14 +0700

    Select Position

---
+++ README.md
@@ -0,0 +1,16 @@
+# TURTLESIM GOTOGOAL
+## Question
+Write the python script to control the TurtleSim to 4 differenct position
+The TurteSim moves to point A > B > C > D > A
+
+The positions:
+A(x,y) = (5,5)
+B(x,y) = (9,5)
+C(x,y) = (6.5,9.6)
+D(x,y) = (1.7,8.5)
+
+Error ~= 0.25
+## Run script
+```sh
+$ roslaunch turtlesim_gotogoal turtlesim_gotogoal.launch
+```
---
+++ scripts/turtlesim_gotogoal.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import cmd
+import math
+from re import T
+from turtle import distance
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+from math import atan2, sqrt, pow, pi
+import time
+
+cmd_vel_angular_ = Twist()
+cmd_vel_linear_ = Twist()
+turtle_pose_ = Pose()
+
+def commandPostitionCallback(position):
+    global turtle_pose_
+    turtle_pose_ = position
+
+def gotogoal(x, y):
+    global cmd_vel_linear_, cmd_vel_angular_
+
+    displacement = sqrt(pow(turtle_pose_.x - x, 2) + pow(turtle_pose_.y - y, 2))
+    angular = atan2(abs(turtle_pose_.y - y), abs(turtle_pose_.x - x))
+
+    if x > turtle_pose_.x :
+        if y >= turtle_pose_.y :
+            angular = angular
+        else :
+            angular = -1 * angular
+    else :
+        if y >= turtle_pose_.y :
+            angular = pi - angular
+        else :
+            angular = (-1 * pi) + angular
+
+    cmd_vel_angular_.angular.z = angular - turtle_pose_.theta
+    cmd_vel_linear_.linear.x = displacement
+
+    print("x: %f y: %f displacement: %f angular: %f" % (x, y, cmd_vel_linear_.linear.x, cmd_vel_angular_.angular.z))
+    
+
+def main():
+    rospy.init_node('turtlesim_gotogoal_node', anonymous=True)
+    rate = rospy.Rate(10)
+
+    cmd_vel_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    tuetle_pose_sub = rospy.Subscriber('/turtle1/pose', Pose, commandPostitionCallback)
+
+    while not rospy.is_shutdown():
+
+        gotox = float(input("Set your x goal:"))
+        gotoy = float(input("Set your y goal:"))
+        gotogoal(gotox, gotoy)
+
+        cmd_vel_pub.publish(cmd_vel_angular_)
+        time.sleep(3)
+
+        cmd_vel_pub.publish(cmd_vel_linear_)
+        print("REACHED THE DESTINATION x = %f, y = %f" % (gotox,gotoy))
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInitException:
+        pass
+
